872de174a242827c9f9b53f223958c6d0bb983f7
afefbalti <afef.balti@esprit.tn>
2020-11-21 01:08:44 +0100

    update Junit

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.assertNotNull;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
 import tn.esprit.spring.services.IEntrepriseService;
 
 @SpringBootTest
@@ -23,9 +24,19 @@ public class EntrepriseServiceImplTest {
 	Integer a, b;
 
 	@Test
+	public void testAjouterEntreprise() {
+
+		Entreprise ent = new Entreprise("ALTICIS", "Enacer");
+		a = entrepriseService.ajouterEntreprise(ent);
+		assertNotNull(a);
+
+	}
+
+
+	@Test
 	public void testAjouterDepartement() {
 
-		Departement dep = new Departement("ooredoo");
+		Departement dep = new Departement("RH");
 		b = entrepriseService.ajouterDepartement(dep);
 		assertNotNull(b);
 	}
@@ -38,10 +49,10 @@ public class EntrepriseServiceImplTest {
 	}
 
 	@Test
-    public void testAffecterDepartementAEntreprise(){
+	public void testAffecterDepartementAEntreprise() {
 
-        entrepriseService.affecterDepartementAEntreprise(26,15);
+		entrepriseService.affecterDepartementAEntreprise(26, 15);
 
-    }
+	}
 
 }
